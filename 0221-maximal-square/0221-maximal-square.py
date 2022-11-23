33efcf5cec4f4b6350f20cc675647312ac858bfe
teja219 <96786916+teja219@users.noreply.github.com>
2022-11-22 20:35:00 -0500

    Time: 2238 ms (9.54%), Space: 87.5 MB (6.20%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def maximalSquare(self, matrix):
+        dp = {}
+        def recur(i,j):
+            if (i,j) in dp:
+                return dp[(i,j)]
+            if i==0 or j==0:
+                if matrix[i][j]=="1":
+                    return 1
+                else:
+                    return 0
+            if matrix[i][j]=="0":
+                dp[(i,j)] = 0
+                return dp[(i,j)]
+            # if i==0:
+            #     dp[(i,j)] = recur(i,j-1)+1
+            #     return dp[(i,j)]
+            # if j==0:
+            #     dp[(i,j)] = recur(i-1,j)+1
+            #     return dp[(i,j)]
+            dp[(i,j)] = min(recur(i-1,j),recur(i,j-1),recur(i-1,j-1))+1
+            return dp[(i,j)]
+        
+        mx = 0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                mx = max(recur(i,j),mx)
+                # print (i,j,recur(i,j))
+            
+        
+        # print(dp)
+        return mx*mx
+        
+                    
+            
+            
+                
+        
+        
