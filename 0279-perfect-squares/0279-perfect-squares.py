9fdf8f73ccd2e65411f84836af24b73f70727174
teja219 <96786916+teja219@users.noreply.github.com>
2022-12-15 23:22:01 -0500

    Time: 5373 ms (23.97%), Space: 27.7 MB (7.60%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    
+    
+    def numSquares(self, n):
+        dp2 = {}
+        x = 1
+        perfectSquares = []
+        while x*x <= 10001:
+            dp2[x*x] = True
+            perfectSquares.append(x*x)
+            x = x +1
+        
+        dp = {}
+        
+        def recur(n):
+            
+            if n in dp:
+                return dp[n]
+            
+            if n in dp2:
+                dp[n] = 1
+                return 1
+            minAns = 10000000000000
+            for x in perfectSquares:
+                if n-x>0:
+                    minAns = min(minAns,1+recur(n-x))
+            dp[n] = minAns
+            return dp[n]
+        
+        return recur(n)
+        
