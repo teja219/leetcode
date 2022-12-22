542ffa412b9bcda9e2ab8bbb81ace9593af49dfb
teja219 <96786916+teja219@users.noreply.github.com>
2022-12-21 20:05:16 -0500

    Time: 584 ms (89.15%), Space: 36.6 MB (11.90%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,51 +1,17 @@
-class MedianFinder(object):
+from heapq import heappush, heappop
 
+class MedianFinder:
     def __init__(self):
-        self.heapLeft = [10000000000]
-        self.heapRight = [10000000000]
-        
-        heapq.heapify(self.heapLeft)
-        heapq.heapify(self.heapRight)
-    
-    def addNum(self, num):
-        leftSize = len(self.heapLeft)
-        rightSize = len(self.heapRight)
-    
-            
-        leftTop = -heapq.heappop(self.heapLeft)
-        rightTop = heapq.heappop(self.heapRight)
-        
-        heapq.heappush(self.heapLeft,-min(leftTop,rightTop,num))
-        heapq.heappush(self.heapRight,max(leftTop,rightTop,num))
-        
-        pivot = leftTop + rightTop + num - min(leftTop,rightTop,num) - max(leftTop,rightTop,num)
-        
-        if rightSize<=leftSize:
-            heapq.heappush(self.heapRight,pivot)
-        else:
-            heapq.heappush(self.heapLeft,-pivot)
-            
-    def findMedian(self):
-        
-        leftSize = len(self.heapLeft)
-        rightSize = len(self.heapRight)
-        # print(self.heapLeft)
-        # print(self.heapRight)
-        
-        leftTop = -self.heapLeft[0]
-        rightTop = self.heapRight[0]
-        
-        # print("%%%%")
-        if leftSize == rightSize:
-            return (leftTop+rightTop)/2.0
-        elif leftSize < rightSize:
-            return rightTop
-        else:
-            return leftTop
-        
+        self.min_heap = []
+        self.max_heap = []
 
+    def addNum(self, num: int) -> None:
+        heappush(self.max_heap, -num)
+        heappush(self.min_heap, -heappop(self.max_heap))
+        if len(self.max_heap) < len(self.min_heap):
+            heappush(self.max_heap, -heappop(self.min_heap))
 
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
+    def findMedian(self) -> float:
+        if len(self.max_heap) > len(self.min_heap):
+            return -self.max_heap[0]
+        return (-self.max_heap[0] + self.min_heap[0]) / 2
