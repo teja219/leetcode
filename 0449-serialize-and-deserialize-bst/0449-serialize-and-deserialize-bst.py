8defaaf30a1df3c328bd0a784cdab01b77e07c2b
teja219 <96786916+teja219@users.noreply.github.com>
2023-01-02 04:51:37 -0500

    Time: 80 ms (77.03%), Space: 21.8 MB (10.81%) - LeetHub

---
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.py
@@ -0,0 +1,63 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        def recurSer(root):
+            if root == None:
+                return ""
+            result = str(root.val)
+            result += ","
+            result = result + recurSer(root.left)
+            result += ","
+            result += recurSer(root.right)
+            return result
+        z = recurSer(root)
+        print(z)
+        return z
+
+    def deserialize(self, data):
+        numbersTemp = data.split(",")
+        numbers = []
+        for x in numbersTemp:
+            if x!="":
+                numbers.append(int(x))
+        print(numbers)
+        def deserializeRecur(numbers):
+            if len(numbers)==0:
+                return None
+            if len(numbers)==1:
+                return TreeNode(numbers[0],None,None)
+            root = numbers[0]
+            rootNode = TreeNode(numbers[0],None,None)
+            
+            
+            found = 0
+            for i in range(1,len(numbers)):
+                if numbers[i]>root:
+                    found = 1
+                    break
+            if found==0:
+                i = i+1
+            rootNode.left = deserializeRecur(numbers[1:i])
+            rootNode.right = deserializeRecur(numbers[i:])
+            return rootNode
+        
+        return deserializeRecur(numbers)
+                        
+        
+    
+        
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# tree = ser.serialize(root)
+# ans = deser.deserialize(tree)
+# return ans
