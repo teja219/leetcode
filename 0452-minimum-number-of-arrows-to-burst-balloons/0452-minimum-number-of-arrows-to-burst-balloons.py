c171efb1b59d868d402c511221623088531ae223
teja219 <96786916+teja219@users.noreply.github.com>
2023-01-06 09:22:49 -0500

    Time: 3903 ms (5.23%), Space: 73.6 MB (15.70%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -2,25 +2,34 @@ class Solution(object):
     def findMinArrowShots(self, points):
         xs = []
         id_ = 0
-        for (x1,x2) in points:
+        points.sort()
+        newpoints = []
+        
+        id_ = 0
+        for [x1,x2] in points:
+            newpoints.append((x1,x2,id_))
+            id_ += 1
+        
+            
+            
+            
+        for (x1,x2,id_) in newpoints:
             xs.append((x1,0,id_))
             xs.append((x2,1,id_))
-            id_ += 1
+            
         
         xs.sort()
         
-        ballonsBurst = set([])
-        currentBaloons = set([])
+        ballonsBurst = -1
+        currentBaloons = -1
         arrowsCount = 0
         for (p,isEnd,id_) in xs:
             if isEnd==0:
-                currentBaloons.add(id_)
-                currentBaloons.add(id_)
+                currentBaloons = id_
             else:
-                if len(currentBaloons)>0 and id_ not in ballonsBurst:
+                if id_ > ballonsBurst:
                     arrowsCount += 1
-                    ballonsBurst.update(currentBaloons)
-                    currentBaloons = set([])
+                    ballonsBurst = currentBaloons
                     
                 
         return arrowsCount
