db18ecf3f7496e8d263a696887f955f411c08590
teja219 <96786916+teja219@users.noreply.github.com>
2023-02-06 23:40:20 -0500

    Time: 4 ms (78.84%), Space: 6.3 MB (88.92%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    bool check(string str) {
-        map<char,int> mp; 
-        for(auto x: str) {
-            mp[x]++;
-        }
-        if(mp.find('a')==mp.end()) return false;
-        if(mp.find('e')==mp.end()) return false;
-        if(mp.find('i')==mp.end()) return false;
-        if(mp.find('o')==mp.end()) return false;
-        if(mp.find('u')==mp.end()) return false;
-        if(mp.size()>5) {return false;}
-        return true;
-    }
     int countVowelSubstrings(string word) {
-        int ans=0; 
-        for(int i=0; i<word.size(); i++) {
-            string str = "";
-            str+=word[i];
-            for(int j=i+1; j<word.size(); j++) {
-                str+=word[j];
-                if(check(str)) {
-                    ans++;
-                }
+        
+        unordered_map<char,int>m;
+        int n=word.size();
+        int ans=0;
+        
+        for(int i=0,cnt=0,j=0,pref=0;i<n;i++){
+            if((word[i]=='a'||word[i]=='e'||word[i]=='i'||word[i]=='o'||word[i]=='u'))
+            {
+                if(m[word[i]]++==0)
+                     cnt++;
+                
+                while(m[word[j]]>1)
+				m[word[j++]]--,pref++;
+                
+            if(cnt==5)ans+=(1+pref);
             }
+               else{
+               
+                   cnt=0;
+                   pref=0;
+                   m.clear();
+                   j=i+1;
+               }
+                   
+            
+            
         }
         return ans;
     }
